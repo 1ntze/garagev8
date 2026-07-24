8b4b3583229759deabe4998ce1d600dd3781a3ab
Nathália Rosendo <nathaliarosend@gmail.com>
2026-07-24 00:16:39 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// Header muda de cor ao descer a página
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 50){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
+
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function trocarSlide(){
+
+    slides[current].classList.remove("active");
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    slides[current].classList.add("active");
+
+}
+
+setInterval(trocarSlide, 5000);
